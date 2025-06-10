64acd65ee80740f96bbc9bcd995d9c432973461f
ArthurKor94 <korchakarthur94@gmail.com>
2025-06-10 10:15:42 +0300

    app init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meine-vokabeltrainer',
+  webDir: 'www'
+};
+
+export default config;
